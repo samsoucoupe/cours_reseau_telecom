ada2ca12b6153ca94eb2d7688a1f84ce3009d48e
samyBendhiab <samybendhiab06@outlook.fr>
2023-01-31 09:19:13 +0100

    add

--- TP1/classe.py
+++ TP1/classe.py
@@ -6,7 +6,19 @@ class Wave:
     Super class that holds the common properties of all wave classes
     """
 
-    def __init__(self, a=1.0, f=440.0, fe=8000.0, ph=0.0, d=1.0, title="Une wave", label="Wave :", format="-bo"):
+    def __init__(
+            self,
+            a=1.0,
+            f=440.0,
+            fe=8000.0,
+            ph=0.0,
+            d=1.0,
+            title="Une wave",
+            label="Wave :",
+            format="-bo",
+    ):
+        self.sig_s = None
+        self.sig_t = None
         self.a = a
         self.f = f
         self.fe = fe
@@ -36,17 +48,31 @@ class Wave:
     def scatter_on_ax(self, ax):
         ax.scatter(self.sig_t, self.sig_s, label=self.label)
 
-    def generate_gaussian_noise(self, mean, std):
-        return np.random.normal(mean, std, len(self.sig_s))
-
-    def generate_impulse_noise(self, mean, std):
-        return np.random.normal(mean, std, len(self.sig_s))
-
-    def add_noise(self, noisy_signal):
-        self.sig_s = self.sig_s + noisy_signal
+    def change_format(self, format):
+        self.format = format
 
     def add_signal(self, signal):
         self.sig_s = self.sig_s + signal.sig_s
+
+    def __add__(self, other):
+        new_signal = Wave()
+
+        self.make_wave()
+        other.make_wave()
+
+        if len(self.sig_t) != len(other.sig_t):
+            raise Exception(
+                "Les deux signaux doivent avoir la meme duree"
+            )
+
+        new_signal.sig_t = self.sig_t
+        sig_s = np.add(self.sig_s, other.sig_s)
+
+        new_signal.title = self.title + " + " + other.title
+        new_signal.make_wave = lambda: sig_s
+        new_signal.sig_s = sig_s
+
+        return new_signal
 
 
 class SinWave(Wave):
@@ -56,7 +82,9 @@ class SinWave(Wave):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.label = "Sin Wave : a={}, f={}, fe={}, ph={}, d={}".format(self.a, self.f, self.fe, self.ph, self.d)
+        self.label = "Sin Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
 
     def wave_type(self, a, omega, t, ph):
         return a * np.sin(omega * t + ph)
@@ -69,7 +97,9 @@ class SquareWave(Wave):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.label = "Square Wave : a={}, f={}, fe={}, ph={}, d={}".format(self.a, self.f, self.fe, self.ph, self.d)
+        self.label = "Square Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
 
     def wave_type(self, a, omega, t, ph):
         return a * (-1) ** np.floor(2 * self.f * t) + ph
@@ -82,10 +112,15 @@ class TriangleWave(Wave):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.label = "Triangle Wave : a={}, f={}, fe={}, ph={}, d={}".format(self.a, self.f, self.fe, self.ph, self.d)
+        self.label = "Triangle Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
 
     def wave_type(self, a, omega, t, ph):
-        return 4 * a * (np.abs(t * self.f - np.floor(t * self.f + 0.5)) - 1.0) + ph
+        return a * (
+                (4 * abs((t + ph) / (1 / self.f) - np.floor(t / (1 / self.f) + 1 / 2)))
+                - 1.0
+        )
 
 
 class SawTeethWave(Wave):
@@ -95,7 +130,60 @@ class SawTeethWave(Wave):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.label = "Saw Teeth Wave : a={}, f={}, fe={}, ph={}, d={}".format(self.a, self.f, self.fe, self.ph, self.d)
+        self.label = "Saw Teeth Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
 
     def wave_type(self, a, omega, t, ph):
         return 2 * a * (t * self.f - np.floor(t * self.f) - 0.5) + ph
+
+
+class GaussianWave(Wave):
+    """
+    Class for gaussian wave
+    """
+
+    def __init__(self, mean, std, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label = "Gaussian Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
+
+        self.mean = mean
+        self.std = std
+
+    def wave_type(self, a, omega, t, ph):
+        return np.random.normal(self.mean, self.std, t.shape)
+
+
+class ImpulseWave(Wave):
+    """
+    Class for impulse wave
+    """
+
+    def __init__(self, mean, std, nbi, di, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label = "Impulse Wave : a={}, f={}, fe={}, ph={}, d={}".format(
+            self.a, self.f, self.fe, self.ph, self.d
+        )
+        self.mean = mean
+        self.std = std
+        self.nbi = nbi  # nombre d'impulsions
+        self.di = di  # durée d'une impulsion
+
+    def make_wave(self):
+        omega = 2 * np.pi * self.f
+        N = int(self.d * self.fe)
+        te = 1.0 / self.fe
+
+        t = np.linspace(0, self.d, N)
+        sig_s = np.zeros(t.shape)
+
+        for i in range(self.nbi):
+            width = self.di
+            length = np.random.normal(self.mean, self.std)
+            pos = np.random.randint(0, N)
+            sig_s[pos:pos + width] = length
+
+        self.sig_t = t
+        self.sig_s = sig_s
--- TP1/main.py
+++ TP1/main.py
@@ -1,80 +1,139 @@
 import math
 
 from Graph_f import Graph
-from classe import SinWave, SquareWave, SawTeethWave, TriangleWave
+from classe import SinWave, SquareWave, SawTeethWave, TriangleWave, GaussianWave, ImpulseWave
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Sinusoidal wave
-    sin_wave = SinWave(a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="--g*")
+    sin_wave = SinWave(
+        a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="--g*"
+    )
     sin_wave.make_wave()
 
-    graph = Graph(waves=[sin_wave], plot_type="line", subplot=False, title="SinWave", xlabel="t", ylabel="Amplitude")
+    graph = Graph(
+        waves=[sin_wave],
+        plot_type="line",
+        subplot=False,
+        title="SinWave",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
     # graph.plot()
 
     # todo2
 
     d = 0.04
-    sin_wave1 = SinWave(a=1.28, f=50, fe=500, ph=0, d=d, title="Deux sinusoide", format="bo")
+    sin_wave1 = SinWave(
+        a=1.28, f=50, fe=500, ph=0, d=d, title="Deux sinusoide", format="bo"
+    )
     sin_wave1.make_wave()
 
-    sin_wave2 = SinWave(a=0.5, f=50, fe=1000, ph=math.pi, d=d, title="Deux sinusoide", format="r.")
+    sin_wave2 = SinWave(
+        a=0.5, f=50, fe=1000, ph=math.pi, d=d, title="Deux sinusoide", format="r."
+    )
     sin_wave2.make_wave()
 
     sin_waves = [sin_wave1, sin_wave2]
 
-    graph = Graph(waves=sin_waves, plot_type="line", subplot=False, title="SinWaves", xlabel="t", ylabel="Amplitude")
+    graph = Graph(
+        waves=sin_waves,
+        plot_type="line",
+        subplot=False,
+        title="SinWaves",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
     # graph.plot()
 
     # todo3 signal carré
 
     d = 0.08
-    sin_square1 = SquareWave(a=1, f=50, fe=1000, ph=0, d=d, title="Un signal carré", format="-bo")
+    sin_square1 = SquareWave(
+        a=1, f=50, fe=1000, ph=0, d=d, title="Un signal carré", format="-bo"
+    )
     sin_square1.make_wave()
 
-    graph = Graph(waves=[sin_square1], plot_type="line", subplot=False, title="SquareWave", xlabel="t",
-                  ylabel="Amplitude")
+    graph = Graph(
+        waves=[sin_square1],
+        plot_type="line",
+        subplot=False,
+        title="SquareWave",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
     # graph.plot()
 
     # todo3.2 signal saw teeth
 
     d = 0.08
-    sin_saw_teeth = SawTeethWave(a=3, f=50, fe=800, ph=0, d=d, title="Un signal saw teeth", format="-bo")
+    sin_saw_teeth = SawTeethWave(
+        a=3, f=50, fe=800, ph=0, d=d, title="Un signal saw teeth", format="-bo"
+    )
     sin_saw_teeth.make_wave()
 
-    graph = Graph(waves=[sin_saw_teeth], plot_type="line", subplot=False, title="SawTeethWave", xlabel="t",
-                  ylabel="Amplitude")
+    graph = Graph(
+        waves=[sin_saw_teeth],
+        plot_type="line",
+        subplot=False,
+        title="SawTeethWave",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
     # graph.plot()
 
     # todo3.3 signal triangle
 
     d = 0.08
-    sin_triangle = TriangleWave(a=3, f=50, fe=800, ph=0, d=d, title="Un signal triangle", format="-bo")
+    sin_triangle = TriangleWave(
+        a=3, f=50, fe=800, ph=0, d=d, title="Un signal triangle", format="-bo"
+    )
     sin_triangle.make_wave()
 
-    graph = Graph(waves=[sin_triangle], plot_type="line", subplot=False, title="TriangleWave", xlabel="t",
-                  ylabel="Amplitude")
-    # graph.plot()
+    graph = Graph(
+        waves=[sin_triangle],
+        plot_type="line",
+        subplot=False,
+        title="TriangleWave",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
+    graph.plot()
 
     # sin wave with noise
-    if False:
-        sin_wave = SinWave(a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="-bo")
-        sin_wave_gauss = SinWave(a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="-g.")
-        sin_wave_impulse = SinWave(a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="-g.")
-        sin_wave_sum = SinWave(a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="-r.")
 
-        sin_wave.make_wave()
-        sin_wave_gauss.make_wave()
-        sin_wave_impulse.make_wave()
-        sin_wave_sum.make_wave()
-        noise_gauss = sin_wave_gauss.generate_gaussian_noise(0.5, 0.1)
-        noise_impulse = sin_wave_impulse.generate_impulse_noise(0.5, 0.1)
-        sin_wave_gauss.add_noise(noise_gauss)
-        sin_wave_impulse.add_noise(noise_impulse)
-        sin_wave_sum.add_signal(sin_wave_gauss)
-        sin_wave_sum.add_signal(sin_wave_impulse)
+    a = 2
+    f = 50.0
+    fe = 1000.0
+    ph = 0
+    d = 0.08
 
-        graph = Graph(waves=[sin_wave, sin_wave_gauss, sin_wave_impulse, sin_wave_sum], plot_type="line", subplot=True,
-                      title="SinWave", xlabel="t", ylabel="Amplitude")
-        graph.plot()
+    sin_wave = SinWave(
+        a=2, f=50, fe=1000, ph=0, d=0.08, title="Une sinusoide", format="-bo"
+    )
+    sin_wave.make_wave()
 
+    mean = 0
+    std = 0.2
+    gaussian_wave = GaussianWave(mean=mean, std=std, fe=1000, d=0.08, title="Gaussian noise", format="-g.")
+    gaussian_wave.make_wave()
 
+    mean = 0
+    std = 2 * a
+    nbi = 2
+    di = 2
+
+    impluse = ImpulseWave(mean=mean, std=std, fe=1000, d=0.08, title="Impulse noise", nbi=nbi, di=di, format="-g.")
+    impluse.make_wave()
+
+    sin_wave_sum = sin_wave + gaussian_wave + impluse
+    sin_wave_sum.change_format("-r.")
+    l_wave = [sin_wave, gaussian_wave, impluse, sin_wave_sum]
+    graph = Graph(
+        l_wave,
+        plot_type="line",
+        subplot=True,
+        title="SinWave with noise",
+        xlabel="t",
+        ylabel="Amplitude",
+    )
+    graph.plot()
