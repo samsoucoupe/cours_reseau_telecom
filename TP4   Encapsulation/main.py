252528585e61d2f30c8d7519afa26cfd7112df90
samyBendhiab <samybendhiab06@outlook.fr>
2023-03-13 19:17:45 +0100

    reagencement des fichiers

--- TP4   Encapsulation/main.py
+++
@@ -1,7 +0,0 @@
-from analyze_semantics import analyze_semantics
-from analyze_syntax import analyze_syntax
-
-if __name__ == "__main__":
-    frames = analyze_syntax("XXX.txt")
-    for frame in frames:
-        analyze_semantics(frame)
